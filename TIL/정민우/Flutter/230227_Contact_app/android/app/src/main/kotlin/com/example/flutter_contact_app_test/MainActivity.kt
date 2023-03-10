85cb77f81e14ca71268e1f0931a8e338731b53cf
Dev-Jun125 <dev.jun125@gmail.com>
2023-03-10 17:59:29 +0900

    [MOD] : TIL폴더로변경

---
+++ TIL/정민우/Flutter/230227_Contact_app/android/app/src/main/kotlin/com/example/flutter_contact_app_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_contact_app_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- 정민우/Flutter/230227_Contact_app/android/app/src/main/kotlin/com/example/flutter_contact_app_test/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_contact_app_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
