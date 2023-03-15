2cdc18b6cbec273ef43507ddbe0ec1442cc5c168
JUNG MINU <minu.j.dev@gmail.com>
2023-03-15 16:34:05 +0900

    [ADD] : Flutter 프로젝트 생성

---
+++ mobile/co_cook/android/app/src/main/kotlin/com/teamcook/co_cook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.teamcook.co_cook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mobile/co_cook/lib/styles/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  const CustomColors();
+
+  static const Color redLight = Color(0xfff2e4d9);
+  static const Color redTertiary = Color(0xfff3b5a8);
+  static const Color redSecondary = Color(0xffd95b4f);
+  static const Color redPrimary = Color(0xffd91604);
+  static const Color monotoneBlack = Color(0xff2e2928);
+  static const Color monotoneGray = Color(0xff78706f);
+  static const Color monotoneLightGray = Color(0xffdfdddd);
+  static const Color monotoneLight = Color(0xfffcfcfc);
+  static const Color greenPrimary = Color(0xff3ca833);
+  static const Color brownPrimary = Color(0xff866844);
+}
