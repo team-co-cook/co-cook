010108997bae09378f6e3bac5b9a83ee788fe425
ysu6691 <ysu6691@gmail.com>
2023-03-15 16:42:38 +0900

    [ADD] : 테마 리스트 API 생성

--- backend/src/main/java/com/cocook/controller/HomeController.java
+++ backend/src/main/java/com/cocook/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.cocook.controller;
 
 import com.cocook.dto.ApiResponse;
 import com.cocook.dto.home.RecommendResDto;
+import com.cocook.dto.home.ThemeResDto;
 import com.cocook.service.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/home")
 public class HomeController {
 
-    private HomeService homeService;
+    private final HomeService homeService;
 
     @Autowired
     public HomeController(HomeService homeService) {
@@ -22,9 +23,15 @@ public class HomeController {
     }
 
     @GetMapping("/recommend")
-    public ResponseEntity<ApiResponse<RecommendResDto>> recommend(@RequestHeader("AUTH-TOKEN") String authToken) {
-        RecommendResDto recommendResDto = homeService.recommend(authToken);
+    public ResponseEntity<ApiResponse<RecommendResDto>> getRecommendRecipes(@RequestHeader("AUTH-TOKEN") String authToken) {
+        RecommendResDto recommendResDto = homeService.getRecommendRecipes(authToken);
         return ApiResponse.ok(recommendResDto);
+    }
+
+    @GetMapping("/theme")
+    public ResponseEntity<ApiResponse<ThemeResDto>> getThemes() {
+        ThemeResDto themeResDto = homeService.getThemes();
+        return ApiResponse.ok(themeResDto);
     }
 
 }
--- backend/src/main/java/com/cocook/service/HomeService.java
+++ backend/src/main/java/com/cocook/service/HomeService.java
@@ -2,11 +2,14 @@ package com.cocook.service;
 
 import com.cocook.auth.JwtTokenProvider;
 import com.cocook.dto.home.RecommendResDto;
+import com.cocook.dto.home.ThemeResDto;
 import com.cocook.dto.recipe.RecipeListResDto;
 import com.cocook.entity.Favorite;
 import com.cocook.entity.Recipe;
+import com.cocook.entity.Theme;
 import com.cocook.repository.FavoriteRepository;
 import com.cocook.repository.RecipeRepository;
+import com.cocook.repository.ThemeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,15 +23,17 @@ public class HomeService {
     private final RecipeRepository recipeRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final FavoriteRepository favoriteRepository;
+    private final ThemeRepository themeRepository;
 
     @Autowired
-    public HomeService(RecipeRepository recipeRepository, JwtTokenProvider jwtTokenProvider, FavoriteRepository favoriteRepository) {
+    public HomeService(RecipeRepository recipeRepository, JwtTokenProvider jwtTokenProvider, FavoriteRepository favoriteRepository, ThemeRepository themeRepository) {
         this.recipeRepository = recipeRepository;
         this.jwtTokenProvider = jwtTokenProvider;
         this.favoriteRepository = favoriteRepository;
+        this.themeRepository = themeRepository;
     }
 
-    public RecommendResDto recommend(String authToken) {
+    public RecommendResDto getRecommendRecipes(String authToken) {
         String themeName;
         int currentHour = LocalDateTime.now().getHour();
         if (currentHour >= 4 && currentHour < 9) {
@@ -65,6 +70,11 @@ public class HomeService {
         }
 
         return new RecommendResDto(resultRecipes);
+    }
+
+    public ThemeResDto getThemes() {
+        List<Theme> themes = themeRepository.findAll();
+        return new ThemeResDto(themes);
     }
 
 }
