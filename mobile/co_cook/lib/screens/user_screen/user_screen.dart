0dac862233ce63216e5ec1e96168545f60f1bdba
qorzi <qorzi00@gmail.com>
2023-03-17 11:22:16 +0900

    [MOD] : 유저스크린 버튼 위젯화

--- mobile/co_cook/lib/screens/user_screen/user_screen.dart
+++ mobile/co_cook/lib/screens/user_screen/user_screen.dart
@@ -5,6 +5,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:co_cook/widgets/bottom_nav_bar/bottom_nav_bar.dart';
 import 'package:co_cook/styles/colors.dart';
 import 'package:co_cook/styles/text_styles.dart';
+import 'package:co_cook/widgets/button/button.dart';
 
 class UserScreen extends StatelessWidget {
   const UserScreen({Key? key}) : super(key: key);
@@ -42,110 +43,27 @@ class UserScreen extends StatelessWidget {
       ),
       body: Container(
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+          padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TextButton(
-                onPressed: () {
-                  // Add your onPressed code here!
-                },
-                child: Text(
-                  '내가 찜한 레시피',
-                  style: CustomTextStyles()
-                      .subtitle1
-                      .copyWith(color: CustomColors.monotoneBlack),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Add your onPressed code here!
-                },
-                child: Text(
-                  '내가 작성한 한줄평',
-                  style: CustomTextStyles()
-                      .subtitle1
-                      .copyWith(color: CustomColors.monotoneBlack),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Add your onPressed code here!
-                },
-                child: Text(
-                  '이용약관',
-                  style: CustomTextStyles()
-                      .subtitle1
-                      .copyWith(color: CustomColors.monotoneBlack),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Add your onPressed code here!
-                },
-                child: Text(
-                  '개인정보처리방침',
-                  style: CustomTextStyles()
-                      .subtitle1
-                      .copyWith(color: CustomColors.monotoneBlack),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Add your onPressed code here!
-                },
-                child: Text(
-                  '공지사항',
-                  style: CustomTextStyles()
-                      .subtitle1
-                      .copyWith(color: CustomColors.monotoneBlack),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Add your onPressed code here!
-                },
-                child: Text(
-                  '자주하는 질문',
-                  style: CustomTextStyles()
-                      .subtitle1
-                      .copyWith(color: CustomColors.monotoneBlack),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Add your onPressed code here!
-                },
-                child: Text(
-                  '고객문의',
-                  style: CustomTextStyles()
-                      .subtitle1
-                      .copyWith(color: CustomColors.monotoneBlack),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Add your onPressed code here!
-                },
-                child: Text(
-                  '회원탈퇴',
-                  style: CustomTextStyles()
-                      .subtitle1
-                      .copyWith(color: CustomColors.redPrimary),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Add your onPressed code here!
-                },
-                child: Text(
-                  '로그아웃',
-                  style: CustomTextStyles()
-                      .subtitle1
-                      .copyWith(color: CustomColors.redPrimary),
-                ),
-              ),
+              CustomTextButton(
+                  text: '내가 작성한 한줄평', color: CustomColors.monotoneBlack),
+              CustomTextButton(text: '이용약관', color: CustomColors.monotoneBlack),
+              CustomTextButton(
+                  text: '개인정보처리방침', color: CustomColors.monotoneBlack),
+              CustomTextButton(text: '공지사항', color: CustomColors.monotoneBlack),
+              CustomTextButton(
+                  text: '자주하는 질문', color: CustomColors.monotoneBlack),
+              CustomTextButton(text: '고객문의', color: CustomColors.monotoneBlack),
+              CustomTextButton(text: '회원탈퇴', color: CustomColors.redPrimary),
+              CustomTextButton(
+                  text: '로그아웃',
+                  color: CustomColors.redPrimary,
+                  onPressed: () {
+                    logOut(context: context);
+                  }),
             ],
           ),
         ),
@@ -154,17 +72,43 @@ class UserScreen extends StatelessWidget {
   }
 }
 
+class CustomTextButton extends StatelessWidget {
+  CustomTextButton(
+      {super.key, required this.text, required this.color, this.onPressed});
+
+  final String text;
+  final Color color;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => onPressed,
+      child: Text(
+        text,
+        style: CustomTextStyles().subtitle1.copyWith(color: color),
+      ),
+      style: ButtonStyle(
+        overlayColor: MaterialStateProperty.all<Color>(
+            Colors.transparent), // 기본 터치 효과를 없애는 코드
+      ),
+    );
+  }
+}
+
+// 구글 로그인 정보 지우기
 Future<void> signOutGoogle() async {
   final GoogleSignIn googleSignIn = GoogleSignIn(); // 구글 로그인 함수 불러오기
   await googleSignIn.signOut();
   print('로그아웃!');
 }
 
+// 전체 로그아웃 호출 함수
 void logOut({required BuildContext context}) async {
   final prefs = await SharedPreferences.getInstance();
   final success = await prefs.remove('userData');
   signOutGoogle();
   print('로그인 정보 삭제 완료!');
-  Route login = MaterialPageRoute(builder: (context) => LoginScreen());
+  Route login = MaterialPageRoute(builder: (context) => const LoginScreen());
   Navigator.pushReplacement(context, login);
 }
