b7b846b9e14862cec214bfe2dbc6984621cf7304
ysu6691 <ysu6691@gmail.com>
2023-03-22 13:17:31 +0900

    [FIX] : 충돌 해결

--- backend/src/main/java/com/cocook/repository/AmountRepository.java
+++ backend/src/main/java/com/cocook/repository/AmountRepository.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public interface AmountRepository extends JpaRepository<Amount, Long> {
 
+    Amount findAmountByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+    
     List<Amount> findAmountsByRecipeId(Long recipeIdx);
 
 }
