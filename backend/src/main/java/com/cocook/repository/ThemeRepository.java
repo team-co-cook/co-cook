5b1832add5fc6b301f28cb75e4178a4db4cf026c
ysu6691 <ysu6691@gmail.com>
2023-03-22 22:11:00 +0900

    [FIX] : 테마 목록에 시간대별 테마 제거

--- backend/src/main/java/com/cocook/repository/ThemeRepository.java
+++ backend/src/main/java/com/cocook/repository/ThemeRepository.java
@@ -2,6 +2,8 @@ package com.cocook.repository;
 
 import com.cocook.entity.Theme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,4 +11,8 @@ public interface ThemeRepository extends JpaRepository<Theme, Long> {
     Theme getThemeByThemeName(String themeName);
 
     List<Theme> findAll();
+
+    @Query("SELECT t FROM Theme t where t.themeName NOT IN (:themeNames)")
+    List<Theme> findThemesNotInThemeNames(@Param("themeNames") List<String> themeNames);
+
 }
