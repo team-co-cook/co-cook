ceed5b7b9427bf1d37f211e5e39ffeabc47989f3
임희상 <hslim9400@gmail.com>
2023-03-24 17:27:55 +0900

    [ADD] : 레시피 추천 알고리즘 1, 2

--- backend/src/main/java/com/cocook/repository/ThemeRepository.java
+++ backend/src/main/java/com/cocook/repository/ThemeRepository.java
@@ -10,6 +10,12 @@ import java.util.List;
 public interface ThemeRepository extends JpaRepository<Theme, Long> {
     Theme getThemeByThemeName(String themeName);
 
+    @Query(value = "SELECT th.theme_idx FROM theme th " +
+            "JOIN tag t ON t.theme_idx = th.theme_idx " +
+            "JOIN recipe r ON t.recipe_idx = r.recipe_idx " +
+            "WHERE r.recipe_idx = :recipeIdx ;", nativeQuery = true)
+    List<Long> findThemesIdxByRecipeIdx(@Param("recipeIdx") Long recipeIdx);
+
     List<Theme> findAll();
 
     @Query("SELECT t FROM Theme t where t.themeName NOT IN (:themeNames)")
