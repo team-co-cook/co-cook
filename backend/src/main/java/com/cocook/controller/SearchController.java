c2202785c904c295b4d12e4b402ae9d930a4e9da
윤성운 <ysu6691@gmail.com>
2023-03-29 14:02:49 +0900

    [MOD] : 재료 키워드 검색 기능 수정

--- backend/src/main/java/com/cocook/controller/SearchController.java
+++ backend/src/main/java/com/cocook/controller/SearchController.java
@@ -3,9 +3,7 @@ package com.cocook.controller;
 import com.cocook.dto.ApiResponse;
 import com.cocook.service.SearchService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
