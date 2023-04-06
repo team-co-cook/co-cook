caa84f42021b27b0ffe77edb4738a2c60f379206
윤성운 <ysu6691@gmail.com>
2023-04-06 15:48:59 +0900

    [FIX] : 코사인 유사도 수정

--- backend/src/main/java/com/cocook/service/ListService.java
+++ backend/src/main/java/com/cocook/service/ListService.java
@@ -100,7 +100,7 @@ public class ListService {
         List<Float> keywordVector = fastText.getVector(keyword);
         relatedRecipes.sort(Comparator.comparingDouble(recipe -> -wordSimilarity.getCosineSimilarity(fastText, keywordVector, recipe.getRecipeName())));
         for (Recipe recipe : relatedRecipes) {
-            if ((wordSimilarity.getCosineSimilarity(fastText, keywordVector, recipe.getRecipeName()) > 0.75)) {
+            if ((wordSimilarity.getCosineSimilarity(fastText, keywordVector, recipe.getRecipeName()) > 0.713)) {
                 if (!foundRecipes.contains(recipe)){
                     foundRecipes.add(recipe);
                 }
